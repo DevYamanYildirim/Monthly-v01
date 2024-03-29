031681de365e3dccedfc4aad11e517df8c1dacf9
DevYamanYildirim <152103134+DevYamanYildirim@users.noreply.github.com>
2024-03-29 16:12:07 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -36,28 +36,52 @@ function addItems() {
     }
 
     if (isActive) {
-      marker = "active-marker";
+      checkedAttribute = "checked";
     } else {
-      marker = "passive-marker";
+      checkedAttribute = "";
     }
 
     let card = document.querySelector(".js-itemCard");
-
     let newItem = `<div class="flex-row border-top pt-2 mt-2">
-        <div class="flex ${marker} rounded mr-2"></div>
-        <div class="w-100">
+      <div class="w-100">
         <div class="flex-row w-100 justify-content-between mb-1">
-            <div class="font-text">${definiton}</div>
-            <img src=${arrowSource}>
+          <div class="font-text">${definiton}</div>
+          <div id="switch">
+            <input type="checkbox" name="one" class="js-checkbox ${checkedAttribute}"/>
+          </div>
         </div>
-        <div class="flex-row w-100 justify-content-between ${amountFontColor}">
-            <div class="font-light-sub-text"><img class="pr-2" src="./icons/calendar-event.svg"/>${dueDate}</div>
-            <div>${amount} ${currency}</div>
+        <div class="flex-row w-100 justify-content-between  ${amountFontColor}">
+          <div class="font-light-sub-text">
+            <img class="pr-2" src="./icons/calendar-event.svg" />${dueDate}
+          </div>
+          <div>${amount} ${currency}</div>
         </div>
-        </div>
+      </div>
     </div>`;
     card.innerHTML += newItem;
   });
 }
 
+let slider = document.querySelector(".js-checkbox");
+let card = document.querySelector(".font-text");
+
+slider.addEventListener("click", (event) => {
+  console.log(event.target.checked);
+});
+console.log(slider);
+console.log(card);
 addItems();
+
+// let oldItem = `<div class="flex-row border-top pt-2 mt-2">
+// <div class="flex ${marker} rounded mr-2"></div>
+// <div class="w-100">
+// <div class="flex-row w-100 justify-content-between mb-1">
+//     <div class="font-text">${definiton}</div>
+//     <img src=${arrowSource}>
+// </div>
+// <div class="flex-row w-100 justify-content-between ${amountFontColor}">
+//     <div class="font-light-sub-text"><img class="pr-2" src="./icons/calendar-event.svg"/>${dueDate}</div>
+//     <div>${amount} ${currency}</div>
+// </div>
+// </div>
+// </div>`;
